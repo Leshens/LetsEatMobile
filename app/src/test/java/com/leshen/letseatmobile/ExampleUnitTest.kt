1544b4aebfe1895b171104988f01a72ff586428b
Kacper Kamm <75252260+Leshens@users.noreply.github.com>
2024-02-01 17:45:24 +0100

    naprawione :disappointed:

--- app/src/test/java/com/leshen/letseatmobile/ExampleUnitTest.kt
+++ app/src/test/java/com/leshen/letseatmobile/ExampleUnitTest.kt
@@ -1,17 +1,79 @@
 package com.leshen.letseatmobile
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.leshen.letseatmobile.restaurantPanel.RestaurantPanelModel
+import com.leshen.letseatmobile.restaurantPanel.Menu
+import com.leshen.letseatmobile.restaurantPanel.Review
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import retrofit2.HttpException
+import retrofit2.Response
+import org.robolectric.RobolectricTestRunner
 
-import org.junit.Assert.*
+@RunWith(RobolectricTestRunner::class)
+class RestaurantPanelViewModelTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @Mock
+    lateinit var apiService: ApiService
+
+    @Mock
+    lateinit var loggerMock: Logger
+
+    @InjectMocks
+    lateinit var viewModel: RestaurantPanelViewModel
+
+    @Before
+    fun setup() {
     }
+    @Test
+    fun `fetchDataFromApi should update errorMessage on HTTP 404 error`() = runBlocking {
+
+        val restaurantId = 1
+        `when`(apiService.getRestaurantPanelData(restaurantId)).thenThrow(
+            HttpException(
+                Response.error<RestaurantPanelModel>(
+                    404,
+                    "not found".toResponseBody("text/plain".toMediaTypeOrNull())
+                )
+            )
+        )
+
+
+        viewModel.fetchDataFromApi(restaurantId)
+
+
+        assertNull(viewModel.restaurantData.value)
+        assertEquals("Failed to connect to the server", viewModel.errorMessage.value)
+    }
+
+    @Test
+    fun `fetchDataFromApi should update errorMessage on generic exception`() = runBlocking {
+
+        val restaurantId = 1
+        `when`(apiService.getRestaurantPanelData(restaurantId)).thenThrow(RuntimeException("Some generic exception"))
+
+        viewModel.fetchDataFromApi(restaurantId)
+
+        assertNull(viewModel.restaurantData.value)
+        assertEquals("Failed to connect to the server", viewModel.errorMessage.value)
+    }
+
 }
